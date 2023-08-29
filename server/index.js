ddd29969b3b5d65165f9661a4cebb5121e1bfc0f
Roshan <shahroshan0720@gmail.com>
2023-08-29 17:43:27 +0530

    DB connect

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Starting Project Management Project with Graphql');
 });
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
 
 const PORT = 2525;
-const start = ()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server listening at http://localhost:${PORT}`);
-    })
+const start = () => {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            app.listen(PORT, () => {
+                console.log(`Server listening at http://localhost:${PORT}`);
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
 start();
