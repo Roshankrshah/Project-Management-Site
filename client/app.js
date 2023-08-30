cb778fc996142458fd6ad159cffe48a1724c64a7
Roshan <shahroshan0720@gmail.com>
2023-08-30 21:32:33 +0530

    delete client

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import fetchClients from "./fetchData/clients.js";
 import fetchProjects from "./fetchData/projects.js";
 import addClient from "./mutateData/addClient.js";
 import addProject from "./mutateData/addProject.js";
+import deleteClient from "./mutateData/deleteClient.js";
 
 const tableBody = document.querySelector('.table-body');
 const projectContainer = document.querySelector('.project-container');
 const clientOption = document.querySelector('.client-select');
 const addClientBtn = document.querySelector('.addclientbtn');
 const addProjectBtn = document.querySelector('.addprojectbtn');
+let deleteBtn;
 
 const start = async () => {
     const clients = await fetchClients();
@@ -16,12 +18,12 @@ const start = async () => {
                     <td>${client.name}</td>
                     <td>${client.email}</td>
                     <td>${client.phone}</td>
-                    <td>cancel</td>
+                    <td><button class="delete-button" data-id="${client.id}">Delete</button></td>
                 </tr>`
     }).join('');
 
     tableBody.innerHTML = tableRow;
-    const options = clients.map((client,index)=>{
+    const options = clients.map((client, index) => {
         return `
         <option value="${client.id}">${client.name}</option>`
     }).join('');
@@ -44,6 +46,16 @@ const start = async () => {
     }).join('');
 
     projectContainer.innerHTML = divEle;
+
+    deleteBtn = document.querySelectorAll('.delete-button');
+    deleteBtn.forEach((btn) => {
+        btn.addEventListener('click', async (e) => {
+            //console.log('delete', e.target.dataset.id)
+            const deletedClient = await deleteClient(e.target.dataset.id);
+            alert(`Client with name ${deletedClient.deleteClient.name} deleted`);
+            location.reload();
+        })
+    })
 }
 
 addClientBtn.addEventListener('click', async (e) => {
@@ -51,7 +63,9 @@ addClientBtn.addEventListener('click', async (e) => {
     const name = document.getElementById('clientInputName').value;
     const email = document.getElementById('clientInputEmail').value;
     const phone = document.getElementById('clientInputPhone').value;
-    const resData = await addClient(name,email,phone);
+    const resData = await addClient(name, email, phone);
+    alert('Client Added Successfully');
+    location.reload();
 });
 
 addProjectBtn.addEventListener('click', async (e) => {
@@ -60,9 +74,13 @@ addProjectBtn.addEventListener('click', async (e) => {
     const desc = document.getElementById('projectTextarea1').value;
     const status = document.querySelector('.status-select').value;
     const client = document.querySelector('.client-select').value;
-    const resData = await addProject(name,desc,status,client);
+    const resData = await addProject(name, desc, status, client);
+    alert('Project Added Successfully');
+    location.reload();
     //console.log(name,desc,status,client);
 });
 
-
 start();
+
+
+
